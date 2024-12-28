989f29e59f3fadf4c79f3713118a83abd4845c25
aloooy108 <ali.zaki.alomran@gmail.com>
2024-12-29 02:18:56 +0300

    change displaybook function to display last of array add a function to take input from form add event listener to btn remove test lines

--- script.js
+++ script.js
@@ -12,38 +12,54 @@ function AddBookToLibrary(book) {
     myLibrary.push(book)
 }
 //placeholder for constructing books
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 300, true)
-const LotR = new Book("Lord of the Rings","J.R.R. Tolkien",500,false)
-AddBookToLibrary(hobbit)
-AddBookToLibrary(LotR)
+// const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 300, true)
+// const LotR = new Book("Lord of the Rings", "J.R.R. Tolkien", 500, false)
+// AddBookToLibrary(hobbit)
+// AddBookToLibrary(LotR)
 
 //function to display books
 function DisplayBooks(library) {
-    for (let i = 0; i < library.length; i++) {
+    
         //create new row
         const newRow = document.createElement('tr')
         table.appendChild(newRow)
         //create ele td for title
         const title = document.createElement('td')
-        title.innerHTML = library[i].title
+        title.innerHTML = library[library.length-1].title
         newRow.appendChild(title)
         //create ele td for author
         const author = document.createElement('td')
-        author.innerHTML = library[i].author
+        author.innerHTML = library[library.length-1].author
         newRow.appendChild(author)
         //create ele td for pages
         const pages = document.createElement('td')
-        pages.innerHTML = library[i].pages
+        pages.innerHTML = library[library.length-1].pages
         newRow.appendChild(pages)
         //create ele td for read
         const read = document.createElement('td')
-        if(library[i].read==true){
-            read.innerHTML="Yes"
-        } else{
-            read.innerHTML="No"
+        if (library[library.length-1].read == true) {
+            read.innerHTML = "Yes"
+        } else {
+            read.innerHTML = "No"
         }
         newRow.appendChild(read)
-    }
+    
 }
-
-DisplayBooks(myLibrary)
+//a function that takes input from form to add book to array
+//work on progress
+function addBookBTN(library) {
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value
+    let read = document.getElementById('read').checked
+    const newBook = new Book(title, author, pages, read)
+    library.push(newBook)
+    console.log('123')
+}
+//when button clicked it adds a book and displays it
+let btn = document.getElementById('addBook')
+btn.addEventListener("click", () => {
+    addBookBTN(myLibrary)
+    DisplayBooks(myLibrary)
+}
+)
