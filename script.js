2faa30e4ff6001055af5da5ccb4d4d94cee4cbfb
aloooy108 <ali.zaki.alomran@gmail.com>
2025-01-04 04:24:57 +0300

    add delete function

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-const myLibrary = []
+let myLibrary = []
+let counter=0
 const table = document.getElementById('bookTable')
 //book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    this.id = id
+
 }
-//add book to the array
-function AddBookToLibrary(book) {
-    myLibrary.push(book)
-}
-//placeholder for constructing books
-// const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 300, true)
-// const LotR = new Book("Lord of the Rings", "J.R.R. Tolkien", 500, false)
-// AddBookToLibrary(hobbit)
-// AddBookToLibrary(LotR)
+
 
 //function to display books
 function DisplayBooks(library) {
+    //create new row
+    const newRow = document.createElement('tr')
+    //set attribute for deletion
+    newRow.setAttribute('data-index-number',`${counter}`)
+    table.appendChild(newRow)
+    //create ele td for title
+    const title = document.createElement('td')
+    title.innerHTML = library[library.length-1].title
+    newRow.appendChild(title)
+    //create ele td for author
+    const author = document.createElement('td')
+    author.innerHTML = library[library.length-1].author
+    newRow.appendChild(author)
+    //create ele td for pages
+    const pages = document.createElement('td')
+    pages.innerHTML = library[library.length-1].pages
+    newRow.appendChild(pages)
+    //create ele td for read
+    const read = document.createElement('td')
+    if (library[library.length-1].read == true) {
+        read.innerHTML = "Yes"
+    } else {
+        read.innerHTML = "No"
+    }
+    newRow.appendChild(read)
+
+    const dlt = document.createElement('button')
     
-        //create new row
-        const newRow = document.createElement('tr')
-        table.appendChild(newRow)
-        //create ele td for title
-        const title = document.createElement('td')
-        title.innerHTML = library[library.length-1].title
-        newRow.appendChild(title)
-        //create ele td for author
-        const author = document.createElement('td')
-        author.innerHTML = library[library.length-1].author
-        newRow.appendChild(author)
-        //create ele td for pages
-        const pages = document.createElement('td')
-        pages.innerHTML = library[library.length-1].pages
-        newRow.appendChild(pages)
-        //create ele td for read
-        const read = document.createElement('td')
-        if (library[library.length-1].read == true) {
-            read.innerHTML = "Yes"
-        } else {
-            read.innerHTML = "No"
-        }
-        newRow.appendChild(read)
-    
+    dlt.innerHTML = 'Delete'
+    newRow.appendChild(dlt)
+    dlt.addEventListener('click',($newRow,)=>{
+        //remove from array
+        myLibrary=myLibrary.filter(book=>book.id!=newRow.getAttribute('data-index-number'))
+        //remove element
+        newRow.remove()
+        
+    })
+    counter++
 }
 //a function that takes input from form to add book to array
 //work on progress
@@ -52,9 +60,9 @@ function addBookBTN(library) {
     let author = document.getElementById('author').value
     let pages = document.getElementById('pages').value
     let read = document.getElementById('read').checked
-    const newBook = new Book(title, author, pages, read)
+    const newBook = new Book(title, author, pages, read, counter)
     library.push(newBook)
-    console.log('123')
+    
 }
 //when button clicked it adds a book and displays it
 let btn = document.getElementById('addBook')
@@ -63,3 +71,4 @@ btn.addEventListener("click", () => {
     DisplayBooks(myLibrary)
 }
 )
+
